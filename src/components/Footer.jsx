5518c1c96de3c64c38d0182075b00c9708d12877
VermontPhil <vermont.paguiligan@gmail.com>
2020-06-04 11:46:42 +0800

    Removed target blank in 'a' tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
 
     return <footer>
         <div>
-        <a className="footer-link" target="_blank" href="https://github.com/vermont-paguiligan"><i class="fab fa-github"></i>Github</a>
-        <a className="footer-link" target="_blank" href="https://www.facebook.com/vermontphil.paguiligan"><i class="fab fa-facebook-square"></i>Facebook</a>
-        <a className="footer-link" target="_blank" href="https://www.linkedin.com/in/vermont-phil-1726731a5/">Linked<i class="fab fa-linkedin"></i></a>
+        <a className="footer-link" href="https://github.com/vermont-paguiligan"><i class="fab fa-github"></i>Github</a>
+        <a className="footer-link" href="https://www.facebook.com/vermontphil.paguiligan"><i class="fab fa-facebook-square"></i>Facebook</a>
+        <a className="footer-link" href="https://www.linkedin.com/in/vermont-phil-1726731a5/">Linked<i class="fab fa-linkedin"></i></a>
         </div>
         <p className="footer">Copyright © {year} Vermont Phil Paguiligan</p>
     </footer>
