8508662bb73dbbcc570b8ccf8b51b46758a28a26
VermontPhil <vermont.paguiligan@gmail.com>
2020-06-05 00:06:57 +0800

    Change background animation to simple background picture

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,20 +6,7 @@ function Intro() {
     <h1>Hi, I'm Vermont.</h1>
     <p>A Full Stack Software Engineer</p>
     </div>
-
-    <ul className="slideshow">
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    </ul>
     </div>
 }
 
 export default Intro;
-
-{/* <div id="intro">
-        <h1>Hi, I'm Vermont.</h1>
-        <p>A Full Stack Software Engineer</p>
-    </div> */}
